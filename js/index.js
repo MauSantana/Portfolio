5f3a0af02313b2e55d7d09fdf72245fcc980648c
MauSantana <mauricio.santos@bandtec.com.br>
2022-11-10 09:29:58 -0300

    responsividade mobile

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function exibirProjetosAcademicos() {
+    academico.style = "display: block;"
+    profissional.style = "display: none;"
+    estudo.style = "display: none;"
+}
+
+function exibirProjetosProfissionais() {
+    academico.style = "display: none;"
+    profissional.style = "display: block;"
+    estudo.style = "display: none;"
+}
+
+function exibirProjetosEstudos() {
+    academico.style = "display: none;"
+    profissional.style = "display: none;"
+    estudo.style = "display: block;"
+}
